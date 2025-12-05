16f47a69fda9b227b15a05c35b283a691f05d072
SaFFireGHOST <Koushik.Mupparapu@iiitb.ac.in>
2025-12-05 18:56:40 +0530

    seperate docker containers for each service

---
+++ build_images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Build all docker images
+docker build -f Dockerfile.user -t user-svc:latest .
+docker build -f Dockerfile.station -t station-svc:latest .
+docker build -f Dockerfile.driver -t driver-svc:latest .
+docker build -f Dockerfile.rider -t rider-svc:latest .
+docker build -f Dockerfile.trip -t trip-svc:latest .
+docker build -f Dockerfile.notification -t notification-svc:latest .
+docker build -f Dockerfile.matching -t matching-svc:latest .
+docker build -f Dockerfile.location -t location-svc:latest .
+docker build -f Dockerfile.gateway -t gateway-svc:latest .
